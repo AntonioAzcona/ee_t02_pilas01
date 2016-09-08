4b0bebfc6edd989a1c70eff2fb1c5d3066999667
AntonioAzcona <tom.ramms.15@gmail.com>
2016-09-08 16:37:38 -0500

    ee_t02_pilas01
    Actualización de éste programa donde hago algunas modificaciones

--- ee_t02_pilas01/src/Pila.java
+++ ee_t02_pilas01/src/Pila.java
@@ -6,34 +6,118 @@
 
 /**
  *
- * @author Azcona Gonález José Antonio
+ * @author AZCONA GONZÁLEZ JOSÉ ANTONIO
+ * Materia: Estructura de Datos
+ * Docente: Lic. León Felipe Villavicencio Fernandez
+ * Hora: 10:00 - 11:00
+ * Nombre del trabajo: Tarea02
  */
 public class Pila {
-    String [] pila;
-    Integer apuntador;
+    String [] pila; //Declaro mi arreglo llamado pila
+    Integer tope;//Variables
     Integer tam;
-    Integer aux;
+    String cadena;
     
-    public Pila(Integer tamanio){
-        apuntador = -1;
+    public Pila(Integer tamanio, String cad){ 
+        tope = -1;
         pila = new String[tamanio];
         tam = tamanio;
-        aux = 0;
+        cadena = cad;
+    }
+    public void push(char dato){ //Este metodo me permite insertar en éste caso un caracter entero a mi pila
+        if(tope < tam-1){ 
+            tope++;
+            pila[tope] = ""+dato;
+        }else{
+            System.out.println("Desbordamiento");
+        }
     }
     
-    public void push(String cadena){//En éste método agrego a la pila una cadena y la separo caracteres para ir metiendo cada uno a la pila
-        if(apuntador < tam-1){
-            apuntador++;
-            for(int i = 0; i < pila.length; i++){
-                pila[apuntador] = ""+cadena.charAt(i);
-                
-                System.out.print(cadena.charAt(i));
-            }
+    public char pop(){ //Lo puse char por que así podra quitar caracter por caracter
+        String var = pila[tope];
+        char resultado = 0;
+        if(tope > -1){
+            resultado = var.charAt(0);
+            pila[tope] = null;
+            tope--;
+        }else{
+            System.out.println("Subdesbordamiento");
         }
+        return resultado;
     }
-    public void comparar(){
-        for (int i = 0; i < pila.length; i++) {
-            
+    
+    public String peek(){
+        String resultado = null;
+        if(tope > -1){
+            resultado = pila[tope];
+        }else{
+            System.out.println("Subdesbordamiento");
         }
+        return resultado;
+    }
+    
+    public void comparar(){
+        int valanceado=cadena.length();
+        if(!(valanceado%2==0))
+        {
+            System.out.println("NO");
+            return;
+        }
+        for(int i = 0; i < cadena.length(); i++){
+            if(cadena.charAt(i) == '{'){ 
+               push(cadena.charAt(i));
+            }else{
+               if(cadena.charAt(i) == '}'){
+                   /*if(estaVacia()){
+                       System.out.println("NO");
+                       return;
+                   }*/
+                   if(pop() == '{')
+                   {}else{
+                  System.out.println("NO");
+                  break;
+                  }
+               }
+            }
+            if(cadena.charAt(i) == '['){
+               push(cadena.charAt(i));
+            }else{
+               if(cadena.charAt(i) == ']'){
+                   /*if(estaVacia()){
+                       System.out.println("NO");
+                       return;
+                   }*/
+                  if(pop() == '['){
+                  //System.out.println("Encontro");
+                  }else{
+                  System.out.println("NO");
+                  break;
+                  }
+               }
+            }
+            if(cadena.charAt(i) == '('){
+               push(cadena.charAt(i));
+            }else{
+               if(cadena.charAt(i) == ')'){
+                   /*if(estaVacia()){
+                       System.out.println("NO");
+                       return;
+                   }*/
+                  if(pop() == '('){
+                  //System.out.println("Encontro");
+                  }else{
+                  System.out.println("NO");
+                  break;
+                  }
+               }
+            }
+       }
+        if(!estaVacia())
+            System.out.println("NO");
+        else
+        System.out.println("SI");
+    }
+    public boolean estaVacia(){
+          return tope == -1;
     }
 }
