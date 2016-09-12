1fd0057a95adacb13ba3267255b4fa05e0759dba
AntonioAzcona <tom.ramms.15@gmail.com>
2016-09-12 01:24:40 -0500

    Update Pila.java

--- ee_t02_pilas01/src/Pila.java
+++ ee_t02_pilas01/src/Pila.java
@@ -13,111 +13,117 @@
  * Nombre del trabajo: Tarea02
  */
 public class Pila {
-    String [] pila; //Declaro mi arreglo llamado pila
+
+    String[] pila; //Declaro mi arreglo llamado pila
     Integer tope;//Variables
     Integer tam;
     String cadena;
-    
-    public Pila(Integer tamanio, String cad){ 
+
+    public Pila(Integer tamanio, String cad) {
         tope = -1;
         pila = new String[tamanio];
         tam = tamanio;
         cadena = cad;
     }
-    public void push(char dato){ //Este metodo me permite insertar en éste caso un caracter entero a mi pila
-        if(tope < tam-1){ 
+
+    public void push(char dato) { //Este metodo me permite insertar en éste caso un caracter entero a mi pila
+        if (tope < tam - 1) {
             tope++;
-            pila[tope] = ""+dato;
-        }else{
+            pila[tope] = "" + dato;
+        } else {
             System.out.println("Desbordamiento");
         }
     }
-    
-    public char pop(){ //Lo puse char por que así podra quitar caracter por caracter
+
+    public char pop() { //Lo puse char por que así podra quitar caracter por caracter
         String var = pila[tope];
         char resultado = 0;
-        if(tope > -1){
+        if (tope > -1) {
             resultado = var.charAt(0);
             pila[tope] = null;
             tope--;
-        }else{
+        } else {
             System.out.println("Subdesbordamiento");
         }
         return resultado;
     }
-    
-    public String peek(){
+
+    public String peek() {
         String resultado = null;
-        if(tope > -1){
+        if (tope > -1) {
             resultado = pila[tope];
-        }else{
+        } else {
             System.out.println("Subdesbordamiento");
         }
         return resultado;
     }
-    
-    public void comparar(){
-        int valanceado=cadena.length();
-        if(!(valanceado%2==0))
-        {
+
+    public void comparar() {
+        int valanceado = cadena.length();
+
+        //----------
+        if (valanceado == 0) {//LA PILA CONTIENE AL MENOS 1 ELEMENTO? OSEA QUE NO ESTÁ VACIA
             System.out.println("NO");
             return;
         }
-        for(int i = 0; i < cadena.length(); i++){
-            if(cadena.charAt(i) == '{'){ 
-               push(cadena.charAt(i));
-            }else{
-               if(cadena.charAt(i) == '}'){
-                   /*if(estaVacia()){
-                       System.out.println("NO");
-                       return;
-                   }*/
-                   if(pop() == '{')
-                   {}else{
-                  System.out.println("NO");
-                  break;
-                  }
-               }
-            }
-            if(cadena.charAt(i) == '['){
-               push(cadena.charAt(i));
-            }else{
-               if(cadena.charAt(i) == ']'){
-                   /*if(estaVacia()){
-                       System.out.println("NO");
-                       return;
-                   }*/
-                  if(pop() == '['){
-                  //System.out.println("Encontro");
-                  }else{
-                  System.out.println("NO");
-                  break;
-                  }
-               }
-            }
-            if(cadena.charAt(i) == '('){
-               push(cadena.charAt(i));
-            }else{
-               if(cadena.charAt(i) == ')'){
-                   /*if(estaVacia()){
-                       System.out.println("NO");
-                       return;
-                   }*/
-                  if(pop() == '('){
-                  //System.out.println("Encontro");
-                  }else{
-                  System.out.println("NO");
-                  break;
-                  }
-               }
-            }
-       }
-        if(!estaVacia())
+        //----------
+
+        if (!(valanceado % 2 == 0)) {
             System.out.println("NO");
-        else
-        System.out.println("SI");
+            return;
+        }
+        for (int i = 0; i < cadena.length(); i++) {
+            if (cadena.charAt(i) == '{') {
+                push(cadena.charAt(i));
+            } else if (cadena.charAt(i) == '}') {
+                /*if(estaVacia()){
+                       System.out.println("NO");
+                       return;
+                   }*/
+                if (pop() == '{') {
+                } else {
+                    System.out.println("NO");
+                    return;//break;
+                }
+            }
+            if (cadena.charAt(i) == '[') {
+                push(cadena.charAt(i));
+            } else if (cadena.charAt(i) == ']') {
+                /*if(estaVacia()){
+                       System.out.println("NO");
+                       return;
+                   }*/
+                if (pop() == '[') {
+                    //System.out.println("Encontro");
+                } else {
+                    System.out.println("NO");
+                    return;//break;
+                }
+            }
+            if (cadena.charAt(i) == '(') {
+                push(cadena.charAt(i));
+            } else if (cadena.charAt(i) == ')') {
+                /*if(estaVacia()){
+                       System.out.println("NO");
+                       return;
+                   }*/
+                if (pop() == '(') {
+                    //System.out.println("Encontro");
+                } else {
+                    System.out.println("NO");
+                    return;//break;
+                }
+            }
+        }
+        if (!estaVacia())//LA PILA CONTIENE AL MENOS 1 ELEMENTO? OSEA QUE NO ESTÁ VACIA
+        {
+            System.out.println("NO");
+        } else {
+            System.out.println("SI");
+        }
     }
-    public boolean estaVacia(){
-          return tope == -1;
+
+    public boolean estaVacia() {
+        return tope == -1;
     }
 }
